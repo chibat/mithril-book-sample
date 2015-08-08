a6b81310ae0e47b7be6e55f815a1a4edb9e562f9
t1000leaf <tomofumi.chiba@gmail.com>
2015-08-08 10:42:54 +0900

    -

--- chapter12_typescript/typescript_sample/client_source/app.ts
+++ chapter12_typescript/typescript_sample/client_source/app.ts
@@ -1,38 +1,32 @@
-/// <reference path="mithril.d.ts" />
+/// <reference path="../typings/mithril/mithril.d.ts" />
 
 // モデルクラスTodo: 2つプロパティがある
 class Todo {
     description = m.prop<string>(null);
     done = m.prop(false);
-   
     constructor(data : any) {
         this.description(data.description);
     }
 };
 
-// ビュー・モデル
-// 作成可能か判定するロジック
-var vm = {
-    list: m.prop<Todo[]>([]),
-    description: m.prop(""),
-    add: () => {
-        if (vm.description()) {
-            vm.list().push(new Todo({description: vm.description()}));
-            vm.description("");
+// コントローラ
+class Controller {
+    list = m.prop<Todo[]>([]);
+    description = m.prop("");
+    add = () => {
+        if (this.description()) {
+            this.list().push(new Todo({description: this.description()}));
+            this.description("");
         }
     }
 }
 
-// コントローラ
-function controller() {
-}
-
 // ビュー
-function view() {
+function view(ctrl: Controller) {
     return m("div", [
-        m("input", {onchange: m.withAttr("value", vm.description), value: vm.description()}),
-        m("button", {onclick: vm.add}, "追加"),
-        m("table", vm.list().map(function (task : Todo) {
+        m("input", {onchange: m.withAttr("value", ctrl.description), value: ctrl.description()}),
+        m("button", {onclick: ctrl.add}, "追加"),
+        m("table", ctrl.list().map(function (task : Todo) {
             return m("tr", [
                 m("td", [
                     m("input[type=checkbox]", {onclick: m.withAttr("checked", task.done), value: task.done()})
@@ -43,5 +37,5 @@ function view() {
     ]);
 }
 
-m.mount(document.getElementById("root"), {controller: controller, view: view});
+m.mount(document.getElementById("root"), {controller: () => {return new Controller();}, view: view});
 
